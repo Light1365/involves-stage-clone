a8ebfe262814cb8cbb025b9ef52d8262d3ae7fd9
João Paulo Pinocci Gomes <jppgomes23@gmail.com>
2024-09-13 18:46:05 -0300

    chat icon has been set to fixed

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className=" text-black bg-white flex min-h-screen flex-col items-center justify-between w-full h-full">
       <Header></Header>
       {/* <HeaderPrimaryDropdown /> */}
-      <LpBody></LpBody>
+      {/* <LpBody></LpBody> */}
       <ChatIcon />
       <div>{/* <BrandLogosSection></BrandLogosSection> */}</div>
     </main>
--- components/page-body/chat-icon/index.tsx
+++ components/page-body/chat-icon/index.tsx
@@ -5,13 +5,13 @@ import { IoIosChatbubbles } from "react-icons/io";
 const ChatIcon = () => {
   return (
     <div>
-      <button className="flex flex-row text-white items-center justify-center absolute bottom-4 right-4 bg-blue-500 shadow-md shadow-gray-400 rounded-full w-20 h-20 transition ease-in-out hover:scale-110">
+      <button className="flex flex-row text-white items-center justify-center fixed bottom-4 right-4 bg-blue-500 shadow-md shadow-gray-400 rounded-full w-20 h-20 transition ease-in-out hover:scale-110">
         <IoIosChatboxes style={{ width: 40, height: 40 }} />
       </button>
-      <button className="flex flex-row text-white items-center justify-center absolute bottom-4 right-24 bg-blue-500 shadow-md shadow-gray-400 rounded-full w-20 h-20 transition ease-in-out hover:scale-110">
+      <button className="flex flex-row text-white items-center justify-center fixed bottom-4 right-24 bg-blue-500 shadow-md shadow-gray-400 rounded-full w-20 h-20 transition ease-in-out hover:scale-110">
         <IoChatbox style={{ width: 40, height: 40 }} />
       </button>
-      <button className="flex flex-row text-white items-center justify-center absolute bottom-24 right-24 bg-blue-500 shadow-md shadow-gray-400 rounded-full w-20 h-20 transition ease-in-out hover:scale-110">
+      <button className="flex flex-row text-white items-center justify-center fixed bottom-24 right-24 bg-blue-500 shadow-md shadow-gray-400 rounded-full w-20 h-20 transition ease-in-out hover:scale-110">
         <IoIosChatbubbles style={{ width: 40, height: 40 }} />
       </button>
     </div>
